3195209479333f7e200a22baf784ffc74fb37ba4
theanhphamit <theanhphamit@gmail.com>
2024-04-03 13:42:53 +0700

    fix public layout

--- app/(public)/(routes)/preview/[documentId]/layout.tsx
+++ app/(public)/(routes)/preview/[documentId]/layout.tsx
@@ -1,3 +1,7 @@
-export function PublicLayout({ children }: { children: React.ReactNode }) {
+export default function PublicLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return <div className="h-full dark:bg-[#1f1f1f]">{children}</div>;
 }
